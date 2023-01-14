cc2b223871ab2b233d6180541caa8d3d9cb7dc83
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-14 05:25:17 +0000

    Match mime file using extensions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use mime::{Mime, APPLICATION_OCTET_STREAM};
+use mime::{Mime, APPLICATION_OCTET_STREAM, IMAGE_JPEG, TEXT_PLAIN, VIDEO};
 
 fn main() {
     /****************************************
@@ -16,4 +16,27 @@ fn main() {
         .parse::<Mime>()
         .unwrap_or(APPLICATION_OCTET_STREAM);
     println!("{:?}", default_mime);
+
+    /****************************************
+     * GET MIME TYPE FROM A FILE EXTENSION
+     ****************************************/
+    let files = vec!["image.jpg", "text.json", "another.png"];
+    for file in files {
+        let mime = find_mime_type(file);
+        println!("{:?}", mime);
+    }
+    fn find_mime_type(file: &str) -> Mime {
+        let parts = file.split('.').collect::<Vec<&str>>();
+
+        let result = match parts.last() {
+            Some(ext) => match *ext {
+                "jpg" => mime::IMAGE_JPEG,
+                "png" => mime::IMAGE_PNG,
+                "json" => mime::APPLICATION_JSON,
+                _ => mime::TEXT_PLAIN,
+            },
+            None => APPLICATION_OCTET_STREAM,
+        };
+        return result;
+    }
 }
